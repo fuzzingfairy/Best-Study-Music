e78e3b64ab37fe8330ea08ef6acccfd2410ffc23
alice reuter <alice@wireless-10-148-212-155.public.utexas.edu>
2019-02-24 01:26:59 -0600

    changed server to post focus value aswell as scrape playlist from focus search

--- emacs.el
+++ emacs.el
@@ -2,7 +2,9 @@
   (shell-command "curl 127.0.0.1:5000/start"))
 
 (defun stopmus()
-  (shell-command "curl 127.0.0.1:5000/stop"))
+  (interactive)
+  (shell-command (concat "curl --data 'focus=" (read-string "How focused where you from (1-5): ") "' 127.0.0.1:5000/stop")))
 
 (add-hook 'org-clock-in-hook #'playmus)
 (add-hook 'org-clock-out-hook #'stopmus)
+
--- spot.py
+++ spot.py
@@ -5,13 +5,25 @@ import pickle
 import time
 import thread
 from flask import Flask
+import flask
 
-
+# tries to load saved data
+try:
+    focus = pickle.load(open("stats.pickle","rb"))
+except:
+    # focus playlist to choose from
+    focus = {"spotify:user:spotify:playlist:37i9dQZF1DWWQRwui0ExPn":0,
+         "spotify:user:spotify:playlist:37i9dQZF1DX4sWSpwq3LiO":0,
+         "spotify:user:spotify:playlist:37i9dQZF1DWZeKCadgRdKQ":0,
+         "spotify:user:spotify:playlist:37i9dQZF1DX4PP3DA4J0N8":0,
+         "spotify:user:spotify:playlist:37i9dQZF1DWWTdxbiocWOL":0,
+         "spotify:user:spotify:playlist:37i9dQZF1DWXLeA8Omikj7":0}
+    
     # Log in to spotify
 CACHE = '.spotipyoauthcache'
 CLIENT_ID='XXXXXXX'
-CLIENT_SECRET='XXXXXXXX'
-REDIRECT_URI='XXXXXXX'
+CLIENT_SECRET='XXXXXXX'
+REDIRECT_URI='XXXXXXXX'
 scope   ="""user-read-email
     playlist-read-private
     playlist-modify-private
@@ -35,6 +47,8 @@ spotifyoauth= spotipy.oauth2.SpotifyOAuth(client_id=CLIENT_ID,client_secret=CLIE
 # default to cached token
 tokeninfo = spotifyoauth.get_cached_token()
 
+
+
 # if not cached login to get token
 if not tokeninfo:
     url = spotifyoauth.get_authorize_url()
@@ -42,49 +56,50 @@ if not tokeninfo:
     url = str(input("\nPlease paste the url you were redirected to below\n"))
     responsecode = spotifyoauth.parse_response_code(url)
     tokeninfo = spotifyoauth.get_access_token(responsecode)
+    
+print("adding new playlists")
 
-    # tries to load saved data
-try:
-    focus = pickle.load(open("stats.pickle","rb"))
-except:
-    # focus playlist to choose from
-    focus = {"spotify:user:spotify:playlist:37i9dQZF1DWWQRwui0ExPn":0,
-         "spotify:user:spotify:playlist:37i9dQZF1DX4sWSpwq3LiO":0,
-         "spotify:user:spotify:playlist:37i9dQZF1DWZeKCadgRdKQ":0,
-         "spotify:user:spotify:playlist:37i9dQZF1DX4PP3DA4J0N8":0,
-         "spotify:user:spotify:playlist:37i9dQZF1DWWTdxbiocWOL":0,
-         "spotify:user:chilledcow:playlist:6eHO8aX6lHu3e6bem8r2rq":0,
-         "spotify:user:jigsaw818:playlist:13svul9ibUBdmuVhNkioxN":0,
-         "spotify:user:spotify:playlist:37i9dQZF1DWXLeA8Omikj7":0}
+sp = spotipy.Spotify(tokeninfo['access_token'])
+for i in sp.search("focus",type='playlist')['playlists']['items']:
+    if i['uri'] not in focus.keys():
+       print("adding playlist: " + i['name'])
+       focus[i['uri']] = 0
     
 app = Flask(__name__)
 chosen = ''
 @app.route("/start")
 def start():
     global chosen,tokeninfo
-    refreshtoken(tokeninfo)
+    tokeninfo = refreshtoken(tokeninfo)
     print(focus)
     chosen = random.choice(focus.keys())
     sp = spotipy.Spotify(tokeninfo['access_token'])
+    sp.shuffle(True)
     sp.start_playback(context_uri=chosen)
     return "started music\n"
 
-@app.route("/stop")
+@app.route("/stop",methods=['GET','POST'])
 def stop():
-    global tokeinfo
-    refreshtoken(tokeninfo)
+    global tokeninfo
+    tokeninfo = refreshtoken(tokeninfo)
     sp = spotipy.Spotify(tokeninfo['access_token'])
-    sp.pause_playback()
+    try:
+        sp.pause_playback()
+    except:
+        print("DONE")
     value = focus.get(chosen)
-    x = int(input("how productive did you feel (1-5)? "))
-    focus.update({chosen:x+value})
-    print(focus)
+    if flask.request.method == 'POST':
+        x = int(flask.request.values.get("focus"))
+    else:
+        x = int(input("how productive did you feel (1-5)? "))
+    focus[chosen] = x+value
     pickle.dump(focus,open("stats.pickle","wb"))
     return "stopped music\n"
 
 def refreshtoken(tokeninfo):
     if(spotifyoauth.is_token_expired(tokeninfo)):
-        spotifyoauth.refresh_access_token(tokeninfo['refresh_token'])
+        tokeninfo = spotifyoauth.refresh_access_token(tokeninfo['refresh_token'])
+    return tokeninfo
     
 if __name__ == '__main__':
    app.run()
