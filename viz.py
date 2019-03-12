ae42bbe4c1d2e12d9f360a6f53eba6527b53f974
alicelambda <alicereuterdev@gmail.com>
2019-03-12 17:21:04 -0500

    done

--- viz.py
+++ viz.py
@@ -4,6 +4,9 @@ import operator
 import json
 import spotipy
 import spotipy.util as util
+import sqlite3
+import secret
+
 focus = pickle.load(open("stats.pickle","rb"))
 sorted_x = sorted(focus.items(), key=operator.itemgetter(1))
 names = [i[0] for i in sorted_x][::-1]
@@ -11,9 +14,6 @@ values =[i[1] for i in sorted_x][::-1]
     
     # Log in to spotify
 CACHE = '.spotipyoauthcache'
-CLIENT_ID='XXXXXX'
-CLIENT_SECRET='XXXXXX'
-REDIRECT_URI='XXXXXXXXXXx'
 scope   ="""user-read-email
     playlist-read-private
     playlist-modify-private
@@ -33,16 +33,36 @@ scope   ="""user-read-email
     user-follow-modify
     user-library-modify
     user-library-read"""
-spotifyoauth= spotipy.oauth2.SpotifyOAuth(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri=REDIRECT_URI,scope=scope,cache_path=CACHE)
+spotifyoauth= spotipy.oauth2.SpotifyOAuth(client_id=secret.CLIENT_ID,client_secret=secret.CLIENT_SECRET,redirect_uri=secret.REDIRECT_URI,scope=scope,cache_path=CACHE)
 # default to cached token
 tokeninfo = spotifyoauth.get_cached_token()
 sp = spotipy.Spotify(tokeninfo['access_token'])
+
 playnames = []
 for i in names:
-    username = i.split(':')[2]
-    playlist_id = i.split(':')[4]
-    playnames.append(sp.user_playlist(username, playlist_id)["name"])
-print(playnames)   
-print(values)
+    try:
+        username = i.split(':')[2]
+        playlist_id = i.split(':')[4]
+        playnames.append(sp.user_playlist(username, playlist_id)["name"])
+    except:
+        username = "spotify"
+        playlist_id = i.split(':')[2]
+        playnames.append(sp.user_playlist(username, playlist_id)["name"])
+
+conn = sqlite3.connect('tracks.db')
+c = conn.cursor()
 for i in range(len(playnames)):
-    print(playnames[i] + " " + str(values[i]))
+    print(playnames[i] + " " + str(values[i]) + " names " + str(names[i]))
+    c.execute("INSERT INTO lookup VALUES ('" +names[i]+"','"+ playnames[i] +"');")
+conn.commit()
+conn.close()
+
+def getName(uri):
+    try:
+        username = i.split(':')[2]
+        playlist_id = i.split(':')[4]
+        return sp.user_playlist(username, playlist_id)["name"]
+    except:
+        username = "spotify"
+        playlist_id = i.split(':')[2]
+        return sp.user_playlist(username, playlist_id)["name"]
